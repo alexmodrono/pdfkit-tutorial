1b7437b5b3388b968c69a21487c89c282b5b592d
Alex M <alex@sureservice.es>
2022-08-31 01:18:06 +0200

    Initial commit

---
+++ pdfkit-tutorial/pdfkit_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  pdfkit_tutorialApp.swift
+//  pdfkit-tutorial
+//
+//  Created by Alex Modroño Vara on 30/8/22.
+//
+
+import SwiftUI
+
+@main
+struct pdfkit_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
