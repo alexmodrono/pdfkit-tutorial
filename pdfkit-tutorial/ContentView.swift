cb383af805953f69a623cf59ef09d8f47d66f53c
Alex M <alex@sureservice.es>
2022-08-31 16:40:34 +0200

    Fix PDFView

--- pdfkit-tutorial/ContentView.swift
+++ pdfkit-tutorial/ContentView.swift
@@ -8,111 +8,14 @@
 import SwiftUI
 import PDFKit
 
-struct PDFViewRepresentable: UIViewRepresentable {
-
-    // MARK: – TYPE ALIASES
-    typealias UIViewType = PDFKit.PDFView
-    
-    // MARK: – STORED PROPERTIES
-    let document: PDFDocument
-    
-    // MARK: – INITIALIZERS
-    public init(document: PDFDocument) {
-        self.document = document
-    }
-
-    // MARK: – METHODS
-    func makeUIView(
-        context: Self.Context
-    ) -> Self.UIViewType {
-
-        // Create a `PDFView` and set its `PDFDocument`.
-        let view = PDFKit.PDFView()
-        view.document = document
-        view.autoScales = true
-
-        return view
-    }
-
-    func updateUIView(
-        _ uiView: Self.UIViewType,
-        context: Self.Context
-    ) {
-        // Not used but we still need to implement it to conform to protocol.
-    }
-}
-
-struct PDFView: View {
-    // MARK: – ENUMS
-    enum PDFError: Error, CustomStringConvertible {
-        case invalidUrl, documentReturnedNil
-        
-        public var description: String {
-            switch self {
-            case .invalidUrl:
-                return "The provided URL is not valid."
-            case .documentReturnedNil:
-                return "PDFDocument initializer returned nil."
-            }
-        }
-    }
-    
-    // MARK: – STORED PROPERTIES
-    let url: URL?
-    
-    // MARK: – COMPUTED PROPERTIES
-    var document: PDFDocument {
-        get throws {
-
-            guard let url = url else {
-                throw PDFError.invalidUrl
-            }
-
-            guard let doc = PDFDocument(url: url) else {
-                throw PDFError.documentReturnedNil
-            }
-
-            return doc
-        }
-    }
-    
-    // MARK: – INITIALIZERS
-    init(for url: URL?) {
-        self.url = url
-    }
-    
-    // MARK: – BODY
-    var body: some View {
-        load()
-    }
-    
-    func load() -> AnyView {
-        do {
-            return AnyView(
-                PDFViewRepresentable(
-                    document: try document
-                )
-            )
-        } catch let error as PDFError {
-            return AnyView(
-                Text("\(error.description)")
-                    .foregroundColor(.red)
-            )
-        } catch {
-            return AnyView(
-                Text(error.localizedDescription)
-                    .foregroundColor(.red)
-            )
-        }
-    }
-}
-
 struct ContentView: View {
     var body: some View {
         PDFView(
-            for: Bundle.main.url(
-                forResource: "sat-practice-test-1",
-                withExtension: "pdf"
+            .init(
+                url: Bundle.main.url(
+                    forResource: "sat-practice-test-1",
+                    withExtension: "pdf"
+                )
             )
         )
     }
---
+++ pdfkit-tutorial/PDFView.swift
@@ -0,0 +1,104 @@
+//
+//  PDFView.swift
+//  pdfkit-tutorial
+//
+//  Created by Alex Modroño Vara on 31/8/22.
+//
+
+import Foundation
+import PDFKit
+import SwiftUI
+
+struct PDFViewRepresentable: UIViewRepresentable {
+
+    // MARK: – TYPE ALIASES
+    typealias UIViewType = PDFKit.PDFView
+    
+    // MARK: – STORED PROPERTIES
+    let document: PDFDocument
+    
+    // MARK: – INITIALIZERS
+    public init(document: PDFDocument) {
+        self.document = document
+    }
+
+    // MARK: – METHODS
+    func makeUIView(
+        context: Self.Context
+    ) -> Self.UIViewType {
+
+        // Create a `PDFView` and set its `PDFDocument`.
+        let view = PDFKit.PDFView()
+        view.document = document
+        view.autoScales = true
+
+        return view
+    }
+
+    func updateUIView(
+        _ uiView: Self.UIViewType,
+        context: Self.Context
+    ) {
+        // Not used but we still need to implement it to conform to protocol.
+    }
+}
+
+extension PDFView {
+    class ViewModel: ObservableObject {
+        
+        // MARK: – ENUMS
+        enum State {
+            case loading
+            case success(document: PDFDocument)
+            case failure(error: String)
+        }
+
+        // MARK: – PUBLISHED VARIABLES
+        @Published var state: State = .loading
+        
+        // MARK: – INITIALIZERS
+        init(url: URL?) {
+            load(url: url)
+        }
+        
+        // MARK: - METHODS
+        func load(url: URL?) {
+            guard let url = url else {
+                return self.state = .failure(
+                    error: "The provided URL is not valid"
+                )
+            }
+
+            guard let doc = PDFDocument(url: url) else {
+                return self.state = .failure(
+                    error: "The document returned nil."
+                )
+            }
+            
+            self.state = .success(document: doc)
+        }
+    }
+}
+
+struct PDFView: View {
+    // MARK: – VIEW MODEL
+    @ObservedObject var viewModel: ViewModel
+    
+    // MARK: – INITIALIZERS
+    init(_ viewModel: ViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    // MARK: – BODY
+    var body: some View {
+        VStack {
+            switch viewModel.state {
+            case .failure(let error): Text(error).foregroundColor(.red)
+            case .success(let document): PDFViewRepresentable(document: document)
+            case .loading:
+                ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle())
+            }
+        }
+    }
+}
